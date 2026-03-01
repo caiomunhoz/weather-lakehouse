d58f8780515210c2ce6906031fc0a606401d748a
caiomunhoz <caio.gmunhoz@gmail.com>
2026-03-01 19:23:49 +0000

    feat(silver): explode hourly data into silver mvs

---
+++ src/pipelines/silver/transformations/bronze_to_silver.py
@@ -0,0 +1,46 @@
+from pyspark import pipelines as dp
+from pyspark.sql import functions as F
+from utilities.utils import explode_hourly
+
+BRONZE_PATH = spark.conf.get('bronze_path')
+
+@dp.table(
+    table_properties={'quality': 'silver'}
+)
+def observations():
+    return (
+        spark.read
+        .table(f'{BRONZE_PATH}.observations')
+        .transform(explode_hourly)
+        .select(
+            'latitude',
+            'longitude',
+            'timezone',
+            F.col('hourly_exploded.time').alias('time'),
+            F.col('hourly_exploded.temperature_2m').alias('temperature'),
+            F.col('hourly_exploded.precipitation').alias('precipitation'),
+            F.col('hourly_exploded.apparent_temperature').alias('apparent_temperature'),
+            F.col('hourly_exploded.cloud_cover').alias('cloud_cover')
+        )
+    )
+
+@dp.table(
+    table_properties={'quality': 'silver'}
+)
+def forecasts():
+    return (
+        spark.read
+        .table(f'{BRONZE_PATH}.forecasts')
+        .transform(explode_hourly)
+        .select(
+            'latitude',
+            'longitude',
+            'timezone',
+            F.col('hourly_exploded.time').alias('time'),
+            F.col('hourly_exploded.temperature_2m').alias('temperature'),
+            F.col('hourly_exploded.precipitation').alias('precipitation'),
+            F.col('hourly_exploded.apparent_temperature').alias('apparent_temperature'),
+            F.col('hourly_exploded.cloud_cover').alias('cloud_cover')
+        )
+    )
+
