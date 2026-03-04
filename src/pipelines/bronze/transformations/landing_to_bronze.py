58a7eab0f8bb472d5caf048d8fac1116b7c75398
caiomunhoz <127498717+caiomunhoz@users.noreply.github.com>
2026-03-04 13:05:42 +0000

    refactor(bronze): add schemas module

--- src/pipelines/bronze/transformations/landing_to_bronze.py
+++ src/pipelines/bronze/transformations/landing_to_bronze.py
@@ -1,40 +1,5 @@
 from pyspark import pipelines as dp
-from pyspark.sql.types import (
-    StructType, StructField, DoubleType, 
-    IntegerType, StringType, ArrayType
-)
-
-RAW_SCHEMA = StructType([
-    StructField('latitude', DoubleType(), False),
-    StructField('longitude', DoubleType(), False),
-    StructField('generationtime_ms', DoubleType(), False),
-    StructField('utc_offset_seconds', IntegerType(), False),
-    StructField('timezone', StringType(), False),
-    StructField('timezone_abbreviation', StringType(), False),
-    StructField('elevation', DoubleType(), False),
-    StructField(
-        'hourly_units',
-        StructType([
-            StructField('time', StringType(), False),
-            StructField('temperature_2m', StringType(), False),
-            StructField('precipitation', StringType(), False),
-            StructField('apparent_temperature', StringType(), False),
-            StructField('cloud_cover', StringType(), False)
-        ]),
-        False
-    ),
-    StructField(
-        'hourly',
-        StructType([
-            StructField('time', ArrayType(StringType()), False),
-            StructField('temperature_2m', ArrayType(DoubleType()), False),
-            StructField('precipitation', ArrayType(DoubleType()), False),
-            StructField('apparent_temperature', ArrayType(DoubleType()), False),
-            StructField('cloud_cover', ArrayType(IntegerType()), False)
-        ]),
-        False
-    )
-])
+from utilities.schemas import RAW_SCHEMA
 
 LANDING_PATH = spark.conf.get('landing_path')
 
