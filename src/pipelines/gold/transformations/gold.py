f315bb920f806c8c20bc7f28cb8c7fe48adb88a9
caiomunhoz <caio.gmunhoz@gmail.com>
2026-03-04 19:15:45 +0000

    feat(gold): add forecast_vs_observations mv

--- src/pipelines/gold/transformations/gold.py
+++ src/pipelines/gold/transformations/gold.py
@@ -47,3 +47,46 @@ def weather_daily_summary():
             F.expr('ROUND(AVG(cloud_cover), 2)').alias('avg_cloud_cover')
         )
     )
+
+@dlt.table(
+    table_properties={'quality': 'gold'}
+)
+def forecast_vs_observations():
+    forecasts = spark.read.table(f'{SILVER_PATH}.forecasts').alias('f')
+    observations = spark.read.table(f'{SILVER_PATH}.observations').alias('o')
+
+    return (
+        forecasts
+        .join(
+            observations, 
+            ['latitude', 'longitude', 'time', 'timezone']
+        )
+        .transform(hash_coordinates)
+        .select(
+            'location_id'
+            'latitude',
+            'longitude',
+            'time',
+            'timezone',
+            F.col('o.temperature').alias('observed_temp'),
+            F.col('f.temperature').alias('forecasted_temp'),
+            F.expr(
+                'ROUND(ABS(forecasted_temp - observed_temp), 2)'
+            ).alias('forecast_temp_error'),
+            F.col('o.apparent_temperature').alias('observed_apparent_temp'),
+            F.col('f.apparent_temperature').alias('forecasted_apparent_temp'),
+            F.expr(
+                'ROUND(ABS(forecasted_apparent_temp - observed_apparent_temp), 2)'
+            ).alias('forecast_apparent_temp_error'),
+            F.col('o.precipitation').alias('observed_precipitation'),
+            F.col('f.precipitation').alias('forecasted_precipitation'),
+            F.expr(
+                'ROUND(ABS(forecasted_precipitation - observed_precipitation), 2)'
+            ).alias('forecast_precipitation_error'),
+            F.col('o.cloud_cover').alias('observed_cloud_cover'),
+            F.col('f.cloud_cover').alias('forecasted_cloud_cover'),
+            F.expr(
+                'ABS(forecasted_cloud_cover - observed_cloud_cover)'
+            ).alias('forecast_cloud_cover_error')
+        )
+    )
