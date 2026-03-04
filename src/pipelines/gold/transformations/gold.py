1ac376e1c0536155fa7fa74b6a450e96c85050b7
caiomunhoz <caio.gmunhoz@gmail.com>
2026-03-04 18:54:18 +0000

    feat(gold): add weather_daily_summary mv

--- src/pipelines/gold/transformations/gold.py
+++ src/pipelines/gold/transformations/gold.py
@@ -3,6 +3,7 @@ from pyspark.sql import functions as F
 from utilities.utils import hash_coordinates
 
 SILVER_PATH = spark.conf.get('silver_path')
+GOLD_PATH = spark.conf.get('gold_path')
 
 @dlt.table(
     table_properties={'quality': 'gold'}
@@ -23,3 +24,26 @@ def weather_time_series():
         .transform(hash_coordinates)
     )
 
+@dlt.table(
+    table_properties={'quality': 'gold'}
+)
+def weather_daily_summary():
+    observations = (
+        spark.read
+        .table(f'{GOLD_PATH}.weather_time_series')
+        .filter("source_type = 'observation'")
+    )
+
+    return (
+        observations
+        .withColumn('date', F.to_date('time'))
+        .groupBy('location_id', 'date')
+        .agg(
+            F.expr('ROUND(AVG(temperature), 2)').alias('avg_temperature'),
+            F.max('temperature').alias('max_temperature'),
+            F.min('temperature').alias('min_temperature'),
+            F.expr('ROUND(AVG(apparent_temperature), 2)').alias('avg_apparent_temperature'),
+            F.expr('ROUND(SUM(precipitation), 2)').alias('total_precipitation'),
+            F.expr('ROUND(AVG(cloud_cover), 2)').alias('avg_cloud_cover')
+        )
+    )
